81f55999c9bf4cec472850908c9c07f1cf779b4b
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-03 18:12:59 +0530

    Configure S3

--- pages/api/media.ts
+++ pages/api/media.ts
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import S3 from 'aws-sdk/clients/s3';
+
+const s3 = new S3({
+  apiVersion: "2006-03-01",
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
+  region: process.env.REGION
+})
 
 type Data = {
   name: string
