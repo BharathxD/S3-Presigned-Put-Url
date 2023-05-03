f4878bddb56a724b8ca93dbe9ee7a7b409d54db9
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-03 18:45:02 +0530

    Finshed and tested the Application

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,39 @@
+import axios from "axios";
+import { ChangeEvent, Fragment, useState } from "react";
+
+const uploadToS3 = async (event: ChangeEvent<HTMLFormElement>) => {
+  const formData = new FormData(event.target);
+  const file = formData.get("file");
+  if (!file) return null;
+  // @ts-ignore
+  const fileType = encodeURIComponent(file.type);
+  const { data } = await axios.get(`/api/media?fileType=${fileType}`);
+  const { s3UploadUrl, key } = data;
+  await axios.put(s3UploadUrl, file);
+  return key;
+};
+
 export default function Home() {
-  return <></>;
+  const [key, setKey] = useState<string | null>(null);
+  const handleUploadMedia = async (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const key = await uploadToS3(event);
+    setKey(key);
+  };
+  return (
+    <Fragment>
+      <div>
+        <p>Upload the Media here</p>
+        <form onSubmit={handleUploadMedia}>
+          <input
+            type="file"
+            accept="image/jpg image/jpeg image/png"
+            name="file"
+          />
+          <button type="submit">Upload</button>
+        </form>
+        {key && <p>{key}</p>}
+      </div>
+    </Fragment>
+  );
 }
