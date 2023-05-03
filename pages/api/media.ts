45effbcebd9939cd347db81686a085630041a74d
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-03 18:18:22 +0530

    Configure S3 Params

--- pages/api/media.ts
+++ pages/api/media.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import S3 from 'aws-sdk/clients/s3';
+import { randomUUID } from 'crypto';
 
 const s3 = new S3({
   apiVersion: "2006-03-01",
@@ -18,5 +19,13 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const ext = (req.query.fileType as string).split("/")[1];
+  const Key = `${randomUUID()}.${ext}`;
+  const s3Params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key,
+    Expires: 10,
+    ContentType: `image/${ext}}`
+  }
   res.status(200).json({ name: 'John Doe' })
 }
