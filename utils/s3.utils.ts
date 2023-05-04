573f7ea0fa6e4d7ff38d5ad19450b1f448f3d95e
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-05-04 23:27:41 +0530

    Refactor homepage component for improved performance and readability

--- utils/s3.utils.ts
+++ utils/s3.utils.ts
@@ -1,15 +1,20 @@
 import axios from "axios";
 
-const uploadToS3 = async (file: File) => {
+const uploadToS3 = async (file: File): Promise<string | null> => {
     if (!file || file.name === "") {
         return null;
     }
-    // @ts-ignore
     const fileType = encodeURIComponent(file.type);
-    const { data } = await axios.get(`/api/media?fileType=${fileType}`);
-    const { s3UploadUrl, key } = data;
-    await axios.put(s3UploadUrl, file);
-    return key;
+
+    try {
+        const { data } = await axios.get(`/api/media?fileType=${fileType}`);
+        const { s3UploadUrl, key } = data;
+        await axios.put(s3UploadUrl, file);
+        return key;
+    } catch (error) {
+        console.error("Failed to upload file to S3:", error);
+        return null;
+    }
 };
 
 export default uploadToS3;
